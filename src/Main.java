50a57b31e9154a85ef67f8ef882e97cd95488e7b
SmitaBidwaiD <smitabidwai2509@gmail.com>
2022-06-01 21:00:19 +0530

    issues resolved

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("Hello,eclipse & cogni ");
 		System.out.println("hope i am in cogni");
+		System.out.println("Hii , Its working");
 	}
 
 }
